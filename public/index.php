56d29f27de3ce480b55ad1cad809c3ac7bb896cb
Jeff Bowers <jeff@turnkeysocial.com>
2011-12-27 11:31:30 -0700

    added comments

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+
+
+//added comment in development
+
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
